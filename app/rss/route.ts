c55020345b1f397142b3cb3ee6250fdd7d4c831c
Miujinho <105462862+miujinho@users.noreply.github.com>
2026-08-09 19:25:28 -0300

    arrumados erros de pnpm

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -16,7 +16,7 @@ export async function GET() {
         `<item>
           <title>${post.metadata.title}</title>
           <link>${baseUrl}/blog/${post.slug}</link>
-          <description>${post.metadata.summary || ''}</description>
+          <description>${post.metadata || ''}</description>
           <pubDate>${new Date(
             post.metadata.publishedAt
           ).toUTCString()}</pubDate>
